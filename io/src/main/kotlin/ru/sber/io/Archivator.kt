682531eea6f2c53d2d4de8dac466c7f0b82434de
Нестеров Никита <mirnes1809@hotmail.com>
2021-09-08 16:10:50 +0300

    ДЗ io на проверку

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,5 +1,10 @@
 package ru.sber.io
 
+import java.io.*
+import java.util.zip.ZipEntry
+import java.util.zip.ZipFile
+import java.util.zip.ZipOutputStream
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
@@ -10,15 +15,47 @@ class Archivator {
      * Метод, который архивирует файл logfile.log в архив logfile.zip.
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
-    fun zipLogfile() {
 
+    fun zipLogFile() {
+        var incomeFile = File("io/logfile.log")
+        var zipFile = File("io/ZipArchive.zip")
+
+        ZipOutputStream(BufferedOutputStream(FileOutputStream(zipFile))).use { out ->
+            if (incomeFile.length() > 1) {
+                FileInputStream(incomeFile).use { input ->
+                    BufferedInputStream(input).use { origin ->
+                        val entry = ZipEntry(incomeFile.name)
+                        out.putNextEntry(entry)
+                        origin.copyTo(out, 1024)
+                    }
+                }
+            }
+        }
     }
+
 
     /**
      * Метод, который извлекает файл из архива.
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
     fun unzipLogfile() {
+        var zipFile = ZipFile("io/ZipArchive.zip")
+        var outComeFile = File("io/unzippedLogfile.log")
 
+        zipFile.use { zip ->
+            zip.entries().asSequence().forEach { entry ->
+                zip.getInputStream(entry).use { input ->
+                    BufferedReader(InputStreamReader(input, "ISO-8859-1")).use { reader ->
+                        BufferedWriter(FileWriter(outComeFile)).use { writer ->
+                            var line: String? = null
+                            while ({ line = reader.readLine(); line }() != null) {
+                                writer.append(line).append('\n')
+                            }
+                            writer.flush()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
